453de68d6e9f66e78c67715eb4f9a1d7a045bda3
dredly <dredly@gmail.com>
2022-02-22 19:50:04 +0000

    Ex1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -11,30 +11,32 @@ const Content = (props) => (
 )
 
 const Total = (props) => (
-  <p>Total exercises: {props.exercises.reduce((a, b) => a + b)}</p>
+  <p>Total exercises: {props.parts.map(part => part.exercises).reduce((a, b) => a + b)}</p>
 )
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total exercises={parts.map(part => part.exercises)} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
