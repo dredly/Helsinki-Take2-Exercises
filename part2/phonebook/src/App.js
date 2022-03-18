c8e059f0eef558888f3e60552b3b570bd63bb758
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-03-18 22:52:49 +0000

    Ex2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const People = ({ personsArray }) => {
+  return (
+    <>
+      {personsArray.map(person => <p key={person.name}>{person.name}</p>)}
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('');
+
+  const handleChange = evt => {
+    setNewName(evt.target.value);
+  }
+
+  const addPerson = evt => {
+    evt.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName('');
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <People personsArray={persons} />
+    </div>
+  )
+}
+
+export default App;
