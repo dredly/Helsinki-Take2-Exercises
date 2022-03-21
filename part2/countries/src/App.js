591539cb14462903ebdaae1d54721e6b950f5616
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-03-21 15:41:26 +0000

    Ex2.12

---
+++ part2/countries/src/App.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const CountrySearch = ({ search, handleChange }) => {
+  return (
+    <>
+      <label htmlFor="country-search-input">find countries </label>
+      <input value={search} id="country-search-input" onChange={handleChange} />
+    </>
+  )
+}
+
+const CountryList = ({ countries }) => {
+  return (
+    <ul>
+      {countries.map(result => <li key={result.cca3}>{result.name.common}</li>)}
+    </ul>
+  )
+}
+
+const CountryDetails = ({ country }) => {
+  return (
+    <>
+      <h1>{country.name.common}</h1>
+      <p>capital {country.capital[0]}</p>
+      <p>area {country.area}</p>
+      <h3>Languages</h3>
+      <ul>
+        {Object.keys(country.languages).map(langKey => (
+          <li key={langKey}>{country.languages[langKey]}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} />
+    </>
+  )
+}
+
+const SearchOutcome = ({ results }) => {
+  if (results.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  }
+  if (results.length > 1 && results.length <= 10) {
+    return <CountryList countries={results} />
+  }
+  if (results.length === 1) {
+    return <CountryDetails country={results[0]} />
+  }
+  return <p>No results</p>
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+        setResults(response.data);
+      })
+  }, []);
+
+  const handleChange = evt => {
+    setSearch(evt.target.value);
+    setResults(countries.filter(country => (
+      country.name.common.toLowerCase().includes(evt.target.value.toLowerCase())
+    )));
+    // console.log(results.map(result => result.name.common));
+  }
+
+  return (
+    <div>
+      <CountrySearch search={search} handleChange={handleChange} />
+      <SearchOutcome results={results} />
+    </div>
+  );
+}
+
+export default App;
