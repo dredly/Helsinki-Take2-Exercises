0bf3472b94559db107739c5a2b142ab170929267
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-03-22 09:07:54 +0000

    Ex2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -33,6 +33,7 @@ const CountryDetails = ({ country }) => {
         ))}
       </ul>
       <img src={country.flags.png} />
+      <Weather country={country} />
     </>
   )
 }
@@ -50,6 +51,46 @@ const SearchOutcome = ({ results, handleShow }) => {
     return <CountryDetails country={results[0]} />
   }
   return <p>No results</p>
+}
+
+const Temperature = ({ temperature }) => (
+  <p>Temperature {parseFloat(temperature).toFixed(2)} celsius</p>
+)
+
+const Wind = ({ wind }) => (
+  <p>wind {parseFloat(wind).toFixed(2)} m/s</p>
+)
+
+const WeatherIcon = ({ src }) => (
+  <img src={src} />
+)
+
+const Weather = ({ country }) => {
+  const [temperature, setTemperature] = useState('');
+  const [wind, setWind] = useState('');
+  const [iconUrl, setIconUrl] = useState('');
+  const [lat, lng] = country.capitalInfo.latlng;
+  const api_key = process.env.REACT_APP_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api_key}`;
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(response => {
+        setTemperature(response.data.main.temp - 272.15);
+        setWind(response.data.wind.speed);
+        const iconCode = response.data.weather[0].icon;
+        setIconUrl(`https://openweathermap.org/img/wn/${iconCode}@2x.png`);
+      })
+  }, []);
+
+  return (
+    <>
+      <h2>Weather in {country.capital[0]}</h2>
+      <Temperature temperature={temperature} />
+      <WeatherIcon src={iconUrl} />
+      <Wind wind={wind} />
+    </>
+  )
 }
 
 const App = () => {
